f6fc762b2d16e354c9167d64f9ae906cbac73e8f
root <kkbase>
2026-01-14 23:35:47 +0800

    windows

--- fn_linux.go
+++ fn_linux.go
@@ -1,9 +1,12 @@
 //go:build linux
 
+// 一些跨平台函数
+
 package rtdb_api
 
 import "unsafe"
 
+// UTF16PtrFromString 占位用的，此为Windows函数，Linux中不存在
 func UTF16PtrFromString(_path string) (unsafe.Pointer, error) {
 	return nil, nil
 }
--- fn_windows.go
+++ fn_windows.go
@@ -1,5 +1,7 @@
 //go:build windows
 
+// 一些跨平台函数
+
 package rtdb_api
 
 import (
@@ -7,6 +9,7 @@ import (
 	"unsafe"
 )
 
+// UTF16PtrFromString 将一个string转换成Windows中的宽字符
 func UTF16PtrFromString(_path string) (unsafe.Pointer, error) {
 	cPath, err := syscall.UTF16PtrFromString(path)
 	return unsafe.Pointer(cPath), err
