63b93699606b31001818cd41e3792b285503266c
root <kkbase>
2026-01-15 09:08:50 +0800

    func

--- fn_linux.go
+++ fn_linux.go
@@ -4,9 +4,15 @@
 
 package rtdb_api
 
-import "unsafe"
+import (
+	"runtime"
+	"unsafe"
+)
 
 // UTF16PtrFromString 占位用的，此为Windows函数，Linux中不存在
 func UTF16PtrFromString(_path string) (unsafe.Pointer, error) {
+	if runtime.GOOS == "linux" {
+		panic("Linux中无法调用此函数，此函数为占位函数")
+	}
 	return nil, nil
 }
